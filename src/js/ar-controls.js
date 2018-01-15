08a9e17b037d0cadf42db68993279654a0da41af
Justin Nilsson <janilsson60@gmail.com>
2018-01-15 18:09:39 -0500

    Pipe framedata position into camera. Use look controls for orientation

--- src/js/ar-controls.js
+++ src/js/ar-controls.js
@@ -1,17 +1,12 @@
-const socket = io();
-
-const send = (data) => {
-  socket.emit('debug', 'frame data:' + data.position)
-};
-
-AFRAME.registerComponent('ar-controls', {
+AFRAME.registerComponent('ar-position', {
   init: function () {
     this.frameData = new VRFrameData();
-    socket.emit('debug', "in component");
   },
   tick: function () {
     THREE.ARUtils.getARDisplay()
         .then((display) => display.getFrameData(this.frameData))
-        .then(() => send(this.frameData.pose))
+        .then(() => {
+          this.el.setAttribute('position', this.frameData.pose.position.join(' '));
+        })
   }
 });
